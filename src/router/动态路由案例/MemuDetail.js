be50d24903a08b99a8d93750664036fc8c564530
RJKKK <shengunkun@gmail.com>
2021-02-25 17:26:35 +0800

    2021.2.25更新

--- src/router/动态路由案例/MemuDetail.js
+++ src/router/动态路由案例/MemuDetail.js
@@ -16,15 +16,30 @@ export default class MemuDetail extends PureComponent {
   getImageSize(url,size) {
     return `http://a.itying.com/${url}?param=${size}`
   }
-  
-  componentDidMount() {
-    const v_id = this.props.match.params.id
+  fetchData(id){
+    const v_id = id
     axios.get(`http://a.itying.com/api/productcontent?id=${v_id}`).then(res => {
       this.setState({
         detail_list: {...res.data.result[0]}
       })
     })
   }
+  /*
+  componentWillReceiveProps方法已过时，
+  建议使用getDerivedStateFromProps(nextProps, prevState)
+  代替componentWillReceiveProps()。
+  老版本中的componentWillReceiveProps()方法判断前后两个 props 是否相同，
+  如果不同再将新的 props 更新到相应的 state 上去。
+  这样做一来会破坏 state 数据的单一数据源，导致组件状态变得不可预测，
+  另一方面也会增加组件的重绘次数。
+  * */
+  componentWillReceiveProps(next) {
+  this.fetchData(next.match.params.id)
+  }
+  componentDidMount() {
+    this.fetchData(this.props.match.params.id)
+  }
+
   render() {
     const { detail_list } = this.state
     console.log(this.state.detail_list,'详情页数据');
